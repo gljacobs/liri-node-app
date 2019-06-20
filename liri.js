81e2eca3cf0a8fac78c5894ac01c1c87d2ac2ec7
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-20 15:22:40 -0700

    set up node readin input

--- liri.js
+++ liri.js
@@ -2,5 +2,30 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var axios = require("axios");
+
 var spotify = new Spotify(keys.spotify);
+
+var cmd = process.argv[2];
+
+var input = process.argv[3];
+
+switch (search) {
+    case "concert-this":
+        var URL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        var URl = "http://www.omdbapi.com/?t=" + input + "&apikey=trilogy"
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        console.log("Please enter a valid command.");
+}
+
+
+
+
 
